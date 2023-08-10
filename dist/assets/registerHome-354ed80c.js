edc7e4da35231329e10c91199f7d3041835680ce
Dins2344 <dinson.cd@gmail.com>
2023-08-11 01:08:58 +0530

    build 5

---
+++ dist/assets/registerHome-354ed80c.js
@@ -0,0 +1,1 @@
+import{j as e,O as r}from"./index-12d5c0b2.js";import{H as s}from"./header-8686224d.js";import{u as n}from"./useIsOnline-5e7f116a.js";const a=()=>n()?e.jsxs(e.Fragment,{children:[e.jsx(s,{}),e.jsx(r,{})]}):e.jsx(e.Fragment,{children:e.jsx("p",{children:"your offline"})});export{a as default};
