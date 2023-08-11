9944233d658a0b2c6ea033ca21545eb347517dcd
Dins2344 <dinson.cd@gmail.com>
2023-08-11 11:14:37 +0530

    build 8.1

---
+++ dist/assets/creation-3fcb6ba3.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-22a109d5.js";const t=()=>r.jsx(r.Fragment,{children:r.jsx("p",{children:"create organi"})});export{t as default};
---
+++ dist/assets/registerHome-6c380e52.js
@@ -0,0 +1,1 @@
+import{j as e,O as r}from"./index-22a109d5.js";import{H as s}from"./header-06e06ec6.js";import{u as n}from"./useIsOnline-6cef892c.js";const a=()=>n()?e.jsxs(e.Fragment,{children:[e.jsx(s,{}),e.jsx(r,{})]}):e.jsx(e.Fragment,{children:e.jsx("p",{children:"your offline"})});export{a as default};
