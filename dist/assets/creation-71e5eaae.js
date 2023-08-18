e223d92b667bc808c03ebaa2dc93fb610c6d77ff
Dins2344 <dinson.cd@gmail.com>
2023-08-19 00:34:45 +0530

    bug fix 1

---
+++ dist/assets/creation-71e5eaae.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-9b76c308.js";const t=()=>r.jsx(r.Fragment,{children:r.jsx("p",{children:"create organi"})});export{t as default};
---
+++ dist/assets/registerHome-84acd7de.js
@@ -0,0 +1,1 @@
+import{j as e,O as r}from"./index-9b76c308.js";import{H as s}from"./header-ce062867.js";import{u as n}from"./useIsOnline-1430ceff.js";const a=()=>n()?e.jsxs(e.Fragment,{children:[e.jsx(s,{}),e.jsx(r,{})]}):e.jsx(e.Fragment,{children:e.jsx("p",{children:"your offline"})});export{a as default};
