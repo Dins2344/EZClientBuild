68ea51c8e5d6e4dda9571fdbed5ba7644b5cd4fe
Dins2344 <dinson.cd@gmail.com>
2023-08-23 19:04:42 +0530

    cleared all consoles

---
+++ dist/assets/creation-e6f65453.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-f1de148d.js";const t=()=>r.jsx(r.Fragment,{children:r.jsx("p",{children:"create organi"})});export{t as default};
---
+++ dist/assets/registerHome-33029fb1.js
@@ -0,0 +1,1 @@
+import{j as e,O as r}from"./index-f1de148d.js";import{H as s}from"./header-b77aef72.js";import{u as n}from"./useIsOnline-883ca2ee.js";const a=()=>n()?e.jsxs(e.Fragment,{children:[e.jsx(s,{}),e.jsx(r,{})]}):e.jsx(e.Fragment,{children:e.jsx("p",{children:"your offline"})});export{a as default};
